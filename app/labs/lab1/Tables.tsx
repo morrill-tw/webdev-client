46dca7bf5cd4af28631e0463102fa30882094522
morrill-tw <morrill.tw@gmail.com>
2026-09-23 12:56:38 -0400

    Tables AI task

--- app/labs/lab1/Tables.tsx
+++ app/labs/lab1/Tables.tsx
@@ -30,11 +30,53 @@ export default function Tables() {
             <td align="center">2/17/21</td>
             <td align="right">95</td>
           </tr>
+          <tr>
+            <td>Q4</td>
+            <td align="center">Node.js</td>
+            <td align="center">2/24/21</td>
+            <td align="right">88</td>
+          </tr>
+          <tr>
+            <td>Q5</td>
+            <td align="center">React</td>
+            <td align="center">3/3/21</td>
+            <td align="right">92</td>
+          </tr>
+          <tr>
+            <td>Q6</td>
+            <td align="center">MongoDB</td>
+            <td align="center">3/10/21</td>
+            <td align="right">78</td>
+          </tr>
+          <tr>
+            <td>Q7</td>
+            <td align="center">REST APIs</td>
+            <td align="center">3/17/21</td>
+            <td align="right">91</td>
+          </tr>
+          <tr>
+            <td>Q8</td>
+            <td align="center">Authentication</td>
+            <td align="center">3/24/21</td>
+            <td align="right">84</td>
+          </tr>
+          <tr>
+            <td>Q9</td>
+            <td align="center">Testing</td>
+            <td align="center">3/31/21</td>
+            <td align="right">89</td>
+          </tr>
+          <tr>
+            <td>Q10</td>
+            <td align="center">Deployment</td>
+            <td align="center">4/7/21</td>
+            <td align="right">93</td>
+          </tr>
         </tbody>
         <tfoot>
           <tr>
             <td colSpan={3}>Average</td>
-            <td align="right">90</td>
+            <td align="right">88.5</td>
           </tr>
         </tfoot>
       </table>
